8e3c90de3d9f63873aa6e606bfc08067d3e877bb
Thatphon Chutinan <newclaffly40685@gmail.com>
2022-04-02 16:23:44 +0700

    add route create car include cars from user

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -6,22 +15,91 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
 const body_parser_1 = __importDefault(require("body-parser"));
+const client_1 = require("@prisma/client");
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 const port = process.env.PORT;
 app.use(body_parser_1.default.json());
-app.get('/', (req, res) => {
-    res.send('Express + TypeScript Server');
-});
-app.get('/about', (req, res) => {
-    res.send('Route about called');
-});
-app.post('/about', (req, res) => {
-    var _a, _b;
-    console.log(req.body);
-    const temp = req.body;
-    res.send((_b = ((_a = temp.persons) === null || _a === void 0 ? void 0 : _a[0].age)) !== null && _b !== void 0 ? _b : "Not found");
-});
+// app.use(express.json())
+const prisma = new client_1.PrismaClient();
+app.post("/", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { username, password } = req.body;
+    const user = yield prisma.user.create({
+        data: {
+            username: username,
+            password: password,
+        }
+    });
+    res.json(user);
+}));
+app.post("/createManyUsers", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { userList } = req.body;
+    const user = yield prisma.user.createMany({
+        data: userList
+    });
+    res.json(user);
+}));
+app.post("/createManyCars", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { carList } = req.body;
+    const cars = yield prisma.car.createMany({
+        data: carList
+    });
+    res.json(cars);
+}));
+app.get("/", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const users = yield prisma.user.findMany({
+        include: { cars: true },
+    });
+    res.json(users);
+}));
+app.get("/byId/:id", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const id = req.params.id;
+    const users = yield prisma.user.findUnique({
+        where: {
+            id: Number(id),
+        }
+    });
+    res.json(users);
+}));
+app.put("/", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { id, username, password } = req.body;
+    const updatedUser = yield prisma.user.update({
+        where: {
+            id: id,
+        },
+        data: {
+            username: username,
+            password: password
+        },
+    });
+    res.json(updatedUser);
+}));
+app.delete("/:id", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const id = req.params.id;
+    const deletedUser = yield prisma.user.delete({
+        where: {
+            id: Number(id),
+        },
+    });
+    res.json(deletedUser);
+}));
+// app.get('/', (req: Request, res: Response) => {
+//     res.send('Express + TypeScript Server')
+// })
+// app.get('/about', (req: Request, res: Response) => {
+//     res.send('Route about called')
+// })
+// app.post('/about', (req: Request, res: Response) => {
+//     console.log(req.body)
+//     const temp: {
+//         username: string,
+//         array: number[],
+//         persons?: {
+//             age: string,
+//         }[]
+//     } = req.body
+//     res.send((temp.persons?.[0].age) ?? "Not found")
+// })
 app.listen(port, () => {
     console.log(`[server] is running at https://localhost:${port}`);
 });
--- index.ts
+++ index.ts
@@ -1,33 +1,106 @@
 import express, { Express, Request, Response } from 'express'
 import dotenv from 'dotenv'
 import bodypaser from 'body-parser'
+import { PrismaClient } from "@prisma/client"
 dotenv.config()
 
 const app: Express = express()
 const port = process.env.PORT
+
 app.use(bodypaser.json())
+// app.use(express.json())
+const prisma = new PrismaClient()
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Express + TypeScript Server')
+app.post("/", async (req: Request, res: Response) => {
+    const { username, password } = req.body
+    const user = await prisma.user.create({
+        data: {
+            username: username,
+            password: password,
+        }
+    })
+    res.json(user)
 })
 
-app.get('/about', (req: Request, res: Response) => {
-    res.send('Route about called')
+app.post("/createManyUsers", async (req: Request, res: Response) => {
+    const { userList } = req.body
+    const user = await prisma.user.createMany({
+      data: userList
+    })
+    res.json(user)
 })
 
-app.post('/about', (req: Request, res: Response) => {
-    console.log(req.body)
-
-    const temp: {
-        username: string,
-        array: number[],
-        persons?: {
-            age: string,
-        }[]
-    } = req.body
-    res.send((temp.persons?.[0].age) ?? "Not found")
-
+app.post("/createManyCars", async (req: Request, res: Response) => {
+    const { carList } = req.body
+    const cars = await prisma.car.createMany({
+      data: carList
+    })
+    res.json(cars)
 })
+
+app.get("/", async (req: Request, res: Response) => {
+    const users = await prisma.user.findMany({
+        include:{cars:true},
+    })
+    res.json(users)
+})
+
+
+app.get("/byId/:id", async (req: Request, res: Response) => {
+    const id = req.params.id
+    const users = await prisma.user.findUnique({
+        where:{
+            id:Number(id),
+        }
+    })
+    res.json(users)
+})
+
+app.put("/", async (req: Request, res: Response) => {
+    const { id, username, password } = req.body
+    const updatedUser = await prisma.user.update({
+        where: {
+            id: id,
+        },
+        data: {
+            username: username,
+            password: password
+        },
+    })
+    res.json(updatedUser)
+})
+
+app.delete("/:id", async (req: Request, res: Response) => {
+    const id = req.params.id
+    const deletedUser = await prisma.user.delete({
+        where: {
+            id: Number(id),
+        },
+    })
+    res.json(deletedUser)
+})
+
+// app.get('/', (req: Request, res: Response) => {
+//     res.send('Express + TypeScript Server')
+// })
+
+// app.get('/about', (req: Request, res: Response) => {
+//     res.send('Route about called')
+// })
+
+// app.post('/about', (req: Request, res: Response) => {
+//     console.log(req.body)
+
+//     const temp: {
+//         username: string,
+//         array: number[],
+//         persons?: {
+//             age: string,
+//         }[]
+//     } = req.body
+//     res.send((temp.persons?.[0].age) ?? "Not found")
+
+// })
 
 
 
